9a57f564f2a21f8b84e4fe0834a964803dc382ce
bilalfarid-1 <158550389+bilalfarid-1@users.noreply.github.com>
2024-10-14 15:09:21 +0500

    Update order.java

--- EcommerceProject ACP/EcommerceProject/src/main/java/com/mycompany/ecommerceproject/order.java
+++ EcommerceProject ACP/EcommerceProject/src/main/java/com/mycompany/ecommerceproject/order.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.ecommerceproject;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Abdullah
- */
+
 public class order {
 
     private ArrayList<orderitem> Orderitems = new ArrayList<>();
