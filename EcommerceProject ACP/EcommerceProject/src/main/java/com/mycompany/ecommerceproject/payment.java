509e6dd11f5ce85dfcadd9122f012bdcdd043551
bilalfarid-1 <158550389+bilalfarid-1@users.noreply.github.com>
2024-10-14 15:09:53 +0500

    Update payment.java

--- EcommerceProject ACP/EcommerceProject/src/main/java/com/mycompany/ecommerceproject/payment.java
+++ EcommerceProject ACP/EcommerceProject/src/main/java/com/mycompany/ecommerceproject/payment.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.ecommerceproject;
 
-/**
- *
- * @author Abdullah
- */
+
 public class payment {
     private int id;
     private order order;
