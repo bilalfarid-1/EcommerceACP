16433ee449e7a5de0425bf123985058710b1a6bd
bilalfarid-1 <158550389+bilalfarid-1@users.noreply.github.com>
2024-10-14 15:10:21 +0500

    Update user.java

--- EcommerceProject ACP/EcommerceProject/src/main/java/com/mycompany/ecommerceproject/user.java
+++ EcommerceProject ACP/EcommerceProject/src/main/java/com/mycompany/ecommerceproject/user.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.ecommerceproject;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author Abdullah
- */
+
 public class user {
 
     static Scanner input = new Scanner(System.in);
