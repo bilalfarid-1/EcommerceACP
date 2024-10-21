94c430ef9fd0361e152032acc1f991b6d7d1b021
bilalfarid-1 <158550389+bilalfarid-1@users.noreply.github.com>
2024-10-21 08:04:30 +0500

    Add cart.java

---
+++ EcommerceProject ACP/EcommerceProject/src/main/java/com/mycompany/ecommerceproject/cart.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ecommerceproject;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class cart {
+    private String productname;
+    private int productid;
+    private int quantity;
+}
