11ddf31b8dee634cf2544e340c08924793eb0116
bilalfarid-1 <158550389+bilalfarid-1@users.noreply.github.com>
2024-10-21 08:02:52 +0500

    Update product.java
    update projuct.java class

--- EcommerceProject ACP/EcommerceProject/src/main/java/com/mycompany/ecommerceproject/product.java
+++ EcommerceProject ACP/EcommerceProject/src/main/java/com/mycompany/ecommerceproject/product.java
@@ -1,13 +1,20 @@
 
 package com.mycompany.ecommerceproject;
 
-import java.util.ArrayList;
+import java.io.*;
+import java.util.Scanner;
 
 
 public class product {
 
-    private inventory inventorys;
     private int id;
+
+    public product(){
+        
+    }
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
     private String name;
     private double price;
     private int stockQuantity;
@@ -19,8 +26,23 @@ public class product {
         this.stockQuantity = stockQuantity;
     }
 
-    public inventory getInventorys() {
-        return inventorys;
+        public void viewallproducts() {
+        try (Scanner fileread = new Scanner(new File("productsData.txt"))) {
+            int i = 1; 
+            while (fileread.hasNextLine()) {
+                String line = fileread.nextLine();
+                String[] parts = line.split(",");
+
+                System.out.println("\n******** Product " + i + " ********");
+                System.out.println("ID: " + parts[0]);
+                System.out.println("Name: " + parts[1]);
+                System.out.println("Price: " + parts[2]);
+                System.out.println("Stock: " + parts[3] + "\n");
+                i++; 
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the product file: " + e.getMessage());
+        }
     }
 
     public int getId() {
